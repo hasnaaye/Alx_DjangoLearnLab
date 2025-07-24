d08fe6ae4fad8d366bd8341f09e951dedec09eac
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-07-24 18:44:14 +0100

    Modified a file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book
+from .models import Library
 from django.views.generic import DetailView
 
 def book_list(request):
@@ -13,7 +14,18 @@ def book_list(request):
 class BookDetailView(DetailView):
   """A class-based view for displaying details of a specific book."""
   model = Book
-  template_name = 'relationship_app/list_books.html'
+  template_name = 'books/book_detail.html'
+
+  def get_context_data(self, **kwargs):
+    """Injects additional context data specific to the book."""
+    context = super().get_context_data(**kwargs)  # Get default context data
+    book = self.get_object()  # Retrieve the current book instance
+    context['average_rating'] = book.get_average_rating() 
+
+class LibraryDetailView(DetailView):
+  """A class-based view for displaying details of a specific book."""
+  model = Library
+  template_name = 'relationship_app/library_detail.html'
 
   def get_context_data(self, **kwargs):
     """Injects additional context data specific to the book."""
