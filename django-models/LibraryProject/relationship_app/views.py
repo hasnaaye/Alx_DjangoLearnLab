3b2de4042dc8d1600033e52df9af80d6d6a0f1aa
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-07-24 20:13:01 +0100

    modified the file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
 def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
@@ -12,14 +15,28 @@ def book_list(request):
       return render(request, 'relationship_app/list_books.html', context)
 
 
+def list_books(request):
+   books = Book.objects.all()
+   return render(request, 'relationship_app/list_books.html', {'book_list': books})
 
 class LibraryDetailView(DetailView):
-  """A class-based view for displaying details of a specific book."""
   model = Library
   template_name = 'relationship_app/library_detail.html'
+  context_object_name = 'library'
 
   def get_context_data(self, **kwargs):
     """Injects additional context data specific to the book."""
     context = super().get_context_data(**kwargs)  # Get default context data
     book = self.get_object()  # Retrieve the current book instance
     context['average_rating'] = book.get_average_rating() 
+
+  
+
+class register(CreateView):
+   form_class = UserCreationForm()
+   template_name = 'relationship_app/register.html'
+   success_url = reverse_lazy('login')
+
+class LoginView(LoginView):
+   template_name = 'relationship_app/login.html'
+   redirect_authenticated_user = True
