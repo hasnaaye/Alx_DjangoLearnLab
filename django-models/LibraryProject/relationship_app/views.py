5b3f316822dc9d1c74601153f4aa284b672a2e30
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-07-25 10:49:29 +0100

    modified the file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -49,8 +49,8 @@ def is_admin(user):
     return hasattr(user, 'profile') and user.profile.role == 'Admin'
 
 @user_passes_test(is_admin)
-def admin_dashboard(request):
-    return HttpResponse("Welcome, Admin! This is the admin view.")
+def Admin(request):
+    return render(request, 'relationship_app/admin_view.html')
 
 
 
@@ -58,8 +58,8 @@ def is_librarian(user):
    return hasattr(user, 'profile') and user.profile.role == "librarian"
 
 @user_passes_test(is_librarian)
-def librarian_dasboard(request):
-   return HttpResponse("Welcome, Librarian! This is the librarian view.")
+def Librarian(request):
+   return render(request, 'relationship_app/librarian_view.html')
 
 
 
@@ -67,6 +67,6 @@ def is_member(user):
    return hasattr(user, 'profile') and user.profile.role == "member"
 
 @user_passes_test(is_member)
-def member_dashboard(request):
-   return HttpResponse("Welcome, Member! This is the member view.")
+def Member(request):
+   return render(request, 'relationship_app/member_view.html')
 
