473eccec9e687ca380d05d3be0dba9106af29c34
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-07-25 10:53:42 +0100

    modified the file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -46,7 +46,7 @@ class LoginView(LoginView):
 
 
 def is_admin(user):
-    return hasattr(user, 'profile') and user.profile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.profile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def Admin(request):
