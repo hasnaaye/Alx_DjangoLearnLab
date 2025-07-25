0f291e02ce39909c1b5571d6ec9ee12a86cbae9c
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-07-25 12:40:53 +0100

    added the folder

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser, CustomUserAdmin
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,6 +9,7 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book)
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # Register your models here.
 
