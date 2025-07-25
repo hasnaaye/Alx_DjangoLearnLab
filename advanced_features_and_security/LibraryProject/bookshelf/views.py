9cd240ece8633ef8c4a51d413d495a4ee924ed15
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-07-25 19:14:28 +0100

    file modified

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Book  
+from .forms import ExampleForm
 
 
 @login_required(raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
