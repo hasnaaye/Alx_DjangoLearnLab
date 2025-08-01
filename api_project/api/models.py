50bcfdbbfe033380043122890cfce84b6fe39396
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-01 15:45:08 +0100

    file updated

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
-class Post(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.TextField()
+
+    def __str__(self):
+        return self.title
