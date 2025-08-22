0182eb000e253b99150b5c36b207712733d764f3
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-23 00:21:10 +0100

    modify file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UserUpdateForm
+
 
 # ✅ Registration view
 def register(request):
@@ -17,3 +18,16 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, "registration/profile.html")
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, "registration/profile.html", {"form": form})
+
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name="logout"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
 ]
