81ffe15c7409852093e5a30be1d0c6eddc23b74f
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-24 16:50:29 +0100

    mofify file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path('post/<int:post_pk>/comment/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
-    path('post/<int:post_pk>/comment/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comment/new/', views.CommentCreateView.as_view(), name='comment-create'),
 ]
