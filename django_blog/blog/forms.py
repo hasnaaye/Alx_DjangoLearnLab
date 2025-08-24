58fdb660964f77aaae24fbf11a83286b4c2518d0
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-24 23:53:49 +0100

    add tag moe

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -17,6 +18,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': TagWidget(),
         }
 
 class CommentForm(forms.ModelForm):
