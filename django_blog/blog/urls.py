7df311005889bacf0e5ae68e433a8f7199738ec4
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-24 15:24:08 +0100

    modify file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,18 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
-from blog import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('blog.urls')),
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="registration/logged_out.html"), name="logout"),
-    path("register/", views.register, name="register"),
-    path("profile/", views.profile, name="profile"),
-    path('', views.home, name='home'),  # temporary home view
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+
+    path('post/', views.PostListView.as_view(), name='post-list'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
