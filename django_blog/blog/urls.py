e59036a84fa68b7580ca5a0daa6ecc5933bd2195
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-24 23:58:56 +0100

    modify

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
     path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('search/', views.search_posts, name='search_posts'),
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
+    path('tags/<slug:tag_slug>/', 'PostByTagListView.as_view()', views.posts_by_tag, name='posts_by_tag'),
 ]
