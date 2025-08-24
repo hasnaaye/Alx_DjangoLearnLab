b451b7391a12e9ff6bd982d0db542ac21cf725de
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-24 15:22:50 +0100

    Implement CRUD Operations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,34 +1,78 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, UserUpdateForm
-from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import UserRegisterForm, PostForm
 
-
-
-# ✅ Registration view
+# Create your views here.
 def register(request):
-    if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("login")  # built-in login URL
+            user = form.save()
+            login(request, user)  # auto-login after register
+            messages.success(request, 'Your account has been created!')
+            return redirect('profile')
     else:
-        form = CustomUserCreationForm()
-    return render(request, "registration/register.html", {"form": form})
-
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
 
 @login_required
 def profile(request):
-    if request.method == "POST":
-        form = UserUpdateForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect("profile")
-    else:
-        form = UserUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html')
 
-    return render(request, "registration/profile.html", {"form": form})
 
-def home(request):
-    return HttpResponse("Hello, Blog!")
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+        model = Post
+        template_name = 'blog/post_detail.html'
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy('post-list')
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+        
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # only author can edit
+        
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post-list")
+
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
 
