c783b21ed724b8251accaefc763aa747b6b64725
Hasnaa Yerrou <hasnaeyerrou@gmail.com>
2025-08-25 16:56:06 +0100

    Modify file

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Token(models.Model):
-    bio =  models.CharField(max_length=200)
+    bio =  models.TextField()
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False)
 
